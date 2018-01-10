3ac6fb683bd1d3a21d37eca03633aa7dd0d94332
MERCURY\BK004313322 <benedict.kabahar@gmail.com>
2018-01-11 00:10:19 +0800

    Last Name Property Added - Benedict Kabahar

--- EmployeeDirectory/EmployeeBase.vb
+++ EmployeeDirectory/EmployeeBase.vb
@@ -2,4 +2,20 @@
     Sub AddNewEmployee()
         MsgBox("New employee added")
     End Sub
+
+
+    'Added by: Benedict Kabahar
+    'START - BK
+    Private _lastName As String
+    Public Property LastName() As String
+        Get
+            Return _lastName
+        End Get
+        Set(ByVal value As String)
+            _lastName = value
+        End Set
+    End Property
+    'END - BK
+
+
 End Class
