5748334a532d7c209cc520309d2bc2a62bc354ad
MERCURY\JL004313128 <joel.lavares@gmail.com>
2018-01-10 23:57:47 +0800

    added prop for first name - jlavares

--- EmployeeDirectory/EmployeeBase.vb
+++ EmployeeDirectory/EmployeeBase.vb
@@ -2,4 +2,15 @@
     Sub AddNewEmployee()
         MsgBox("New employee added")
     End Sub
+
+    Private _firstName As String
+    Public Property firstName() As String
+        Get
+            Return _firstName
+        End Get
+        Set(ByVal value As String)
+            _firstName = value
+        End Set
+    End Property
+
 End Class
